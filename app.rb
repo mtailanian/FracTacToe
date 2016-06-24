0736658c68524199c8e9cd2dd16a3e468bb21347
Kleer LA <kleer@kleer-sandbox.(none)>
2016-06-24 12:57:47 -0300

    merge

--- app.rb
+++ app.rb
@@ -12,8 +12,6 @@ get '/' do
 
 	@@pos = Array.new(3){	Array.new(3) {Array.new(3) {Array.new(3, @@ttt.get_empty)}}}
 
-	@@pos[1][0][0][2] = "X"
-
 	erb:fracTacToe
 end
 
@@ -21,17 +19,17 @@ def validate
 	
 	for x in 0..2
 		for y in 0..2
-			@fttt.get_ttts[x][y].set_ttt_str @@pos[x][y][0][0] + @@pos[x][y][0][1] + @@pos[x][y][0][2] + @@pos[x][y][1][0] + @@pos[x][y][1][1] + @@pos[x][y][1][2] + @@pos[x][y][2][0]+ @@pos[x][y][2][1]+ @@pos[x][y][2][2]
+			@@fttt.get_ttts[x][y].set_ttt_str @@pos[x][y][0][0] + @@pos[x][y][0][1] + @@pos[x][y][0][2] + @@pos[x][y][1][0] + @@pos[x][y][1][1] + @@pos[x][y][1][2] + @@pos[x][y][2][0]+ @@pos[x][y][2][1]+ @@pos[x][y][2][2]
 		end
 	end
 
-	@fttt.update_ttt_main_str
-	@@winner = @fttt.get_ttt_main.get_winner
+	@@fttt.update_ttt_main_str
+	@@winner = @@fttt.get_ttt_main.get_winner
 end
 
 get '/:FposX/:FposY/:posX/:posY' do
 	@@turn += 1
-	@@pos[:FposX][:FposY][:posX][:posY] = @@teams[@@turn%2]
+	@@pos[params[:FposX].to_i][params[:FposY].to_i][params[:posX].to_i][params[:posY].to_i] = @@teams[@@turn%2]
 	validate
 	erb :fracTacToe
 end
